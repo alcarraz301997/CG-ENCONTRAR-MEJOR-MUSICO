12e49dfe97804610aa3fa89e92570a6436ff0e94
alcarraz301997 <guillermo.junior.30@gmail.com>
2020-08-23 17:36:41 -0500

    Imprimiendo valores de aptitud-1

--- js/encontrar.js
+++ js/encontrar.js
@@ -4,44 +4,50 @@ let cromosomas = 6;
 let generaciones = 3;
 
 //Creamos un arreglo de 6 x 7
-let poblacion = new Array(cromosomas);
+let poblacion = new Array(individuos);
 
-for (let i = 0; i < cromosomas; i++) {
-  poblacion[i] = new Array(individuos);
+for (let i = 0; i < individuos; i++) {
+  poblacion[i] = new Array(cromosomas);
 }
 
 console.log("POBLACION INICIAL");
 
 //Llenando la población aleatoriamente
-for (let i = 0; i < cromosomas; i++) {
-  for (let j = 0; j < individuos; j++) {
+for (let i = 0; i < individuos; i++) {
+  for (let j = 0; j < cromosomas; j++) {
     poblacion[i][j] = Math.round(Math.random());
   }
 }
 
 //Función para medir aptitud
-function medir_aptitud(poblacion){
-    let aptitud = new Array(individuos);
-    let valores = ["Signo", Math.pow(2, 3), Math.pow(2, 2), Math.pow(2, 1), Math.pow(2, 0), Math.pow(2, -1), Math.pow(2, -2)]
-    console.log("")
-    console.log("VALORES PARA DETERMINAR APTITUD MUSICAL")
-    console.log(valores)
+function medir_aptitud(poblacion) {
+  let aptitud = new Array(individuos);
+  let valores = [
+    Math.pow(2, 3),
+    Math.pow(2, 2),
+    Math.pow(2, 1),
+    Math.pow(2, 0),
+    Math.pow(2, -1),
+    Math.pow(2, -2),
+  ];
+  console.log("");
+  console.log("VALORES PARA DETERMINAR APTITUD MUSICAL");
+  console.log(valores);
 
-    //Multiplicando el Vector de Aptitud por el cromosoma ( fila) de cada individuo de la Poblacion.
-    for (let i = 0; i < individuos; i++) {
-        for (let j = 0; j < individuos; j++) {
-        aptitud[i] += poblacion[i][j] * valores[cromosomas]
-        //Cambiando el signo segun valor
-        if (poblacion[i][0] == 1){
-            aptitud[i] *= -1
-        }
+  //Multiplicando el Vector de Aptitud por el cromosoma ( fila) de cada individuo de la Poblacion.
+  for (let i = 0; i < individuos; i++) {
+    for (let j = 0; j < cromosomas; j++) {
+      aptitud[i] += poblacion[i][j] * valores[j];
+      //Cambiando el signo segun valor
     }
+  }
 
-    //Imprimiendo valores de aptitud de cada  fila o cromosoma
-    console.log("")
-    console.log("APTITUD")
-    for (let k = 0; k < individuos; k++) {
-        console.log(individuo.toString() + " - [" + "")
-    }
+  //Imprimiendo valores de aptitud de cada  fila o cromosoma
+  console.log("");
+  console.log("APTITUD");
+  for (let i = 0; i < individuos; i++) {
+    console.log(i + " - [" + poblacion[i].join(", ") + "] = " + aptitud[i])
+  }
 
 }
+medir_aptitud(poblacion);
